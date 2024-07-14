ddb3b66bdce5cbac67baebc98a2a5847becd7041
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-13 23:11:45 -0300

    Iniciando estudos de java

---
+++ Java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        String Nome = "Jefferson";
+        String Idade = "30";
+
+        System.out.println ("Meu nome é " + Nome +  "!");
+        System.out.println ("Eu tenho " + Idade + " Anos de idade!");
+
+
+
+    }
+    
+}
