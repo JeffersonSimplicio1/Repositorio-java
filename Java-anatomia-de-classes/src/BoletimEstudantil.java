e9292489d1a3c3285d2be32a488a899678cb05c8
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-14 19:25:40 -0300

    Commit novo projeto BoletimEstudantil

---
+++ Java-anatomia-de-classes/src/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+            int MediaFinal = 6;
+        if (MediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (MediaFinal == 6)
+            System.out.println("Prova Final");
+        else
+            System.out.println("Aprovado");
+
+    }
+}
